d74f5d71c776c6560ef75e13a19b42addddaae0e
Studiosnlabs <Studiosnlabs@gmail.com>
2021-01-29 17:42:51 +0000

    Initial commit

---
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.currencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convert(View view){
+
+        EditText dollarAmountEditText =(EditText)  findViewById(R.id.dollarAmtEditText);
+
+        Double dollarAmountDouble = Double.parseDouble(dollarAmountEditText.getText().toString());
+
+        Double ghanacedis = dollarAmountDouble * 5.80;
+
+        Toast.makeText(MainActivity.this,"GHS "+ ghanacedis.toString(),Toast.LENGTH_SHORT).show();
+
+        Log.i("test amount",dollarAmountEditText.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
